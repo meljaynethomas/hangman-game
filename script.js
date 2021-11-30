5718127bd79d24ab68fb64209c546da61665e409
Mel Thomas <meljaynethomas@gmail.com>
2021-11-30 14:51:21 +0000

    amended code to alert user if they win and show an image; removed shadows from images; played around with layout of gamepage section

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const animals = ["fox", "mouse", "elephant", "horse", "giraffe", "flamingo", "ze
 const food = ["fajitas", "toast", "curry", "salad", "sausages", "bananas", "chocolate", "biscuits", "potatoes", "pasta"];
 
 let word = "";
-let displayedAnswerState = document.getElementById("gamepage__currentAnswerState");
+let displayedAnswerState = document.querySelector('.gamepage__currentAnswerState');
 let currentAnswerState = [];
 const maximumGuesses = 7;
 let correctGuesses = 0;
@@ -23,37 +23,37 @@ let numOfGuesses = 0;
 const generateBookWord = () => {
   const randomWord = Math.floor(Math.random() * books.length);
   word = books[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "Books";
 }
 
-function generateMoviesWord() {
+const generateMoviesWord = () => {
   const randomWord = Math.floor(Math.random() * movies.length);
   word = movies[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "Movies";
 }
 
 const generateCountriesWord = () => {
   const randomWord = Math.floor(Math.random() * countries.length);
   word = countries[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "Countries";
 }
 
 const generateBeachWord = () => {
   const randomWord = Math.floor(Math.random() * beach.length);
   word = beach[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "At The Beach";
 }
 
 const generateAnimalsWord = () => {
   const randomWord = Math.floor(Math.random() * animals.length);
   word = animals[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "Animals";
 }
 
 const generateFoodWord = () => {
   const randomWord = Math.floor(Math.random() * food.length);
   word = food[randomWord];
-  // console.log(word)
+  document.querySelector('.gamepage__category').innerHTML = "Food";
 }
 
 const createUserState = () => {
@@ -99,42 +99,30 @@ const updateUserState = (event) => {
   }
 
   //now we work out whether that means that the user has won
-  // let hasWon = true;
-  // for (let i = 0; i < word.length; i++) {
-  //   if(word[i] === '_') {
-  //     hasWon = false;
-  //   }
-  // }
-
-  currentAnswerState.every(checkUnderscore)
-
-  function checkUnderscore(underscore) {
-    if (underscore = '_') {
-      alert("You won!");
-    };
-  }
-
-
-
-
+  let hasWon = true;
+    if(currentAnswerState.includes('_')) {
+      hasWon = false;
+    }
 
   //Now we work out what we need to do based on whether we've won or not
   if(hasWon) {
-    alert("You won!")
-    } else if(incorrectGuesses === 1) {
-      document.getElementById("gamepage__image").src = "/images/img2_head.png";
-    } else if(incorrectGuesses === 2) {
-      document.getElementById("gamepage__image").src = "/images/img3_head_body.png";
-    } else if(incorrectGuesses === 3) {
-      document.getElementById("gamepage__image").src = "/images/img4_one_arm.png";
-    } else if(incorrectGuesses === 4) {
-    document.getElementById("gamepage__image").src = "/images/img5_two_arms.png";
-    } else if(incorrectGuesses === 5) {
-      document.getElementById("gamepage__image").src = "/images/img6_one_leg.png";
-    } else if(incorrectGuesses === 6) {
-      document.getElementById("gamepage__image").src = "/images/img7_lose.png"; 
-      alert("You've Lost!!");
-    } 
+    alert("You've won!")
+    document.querySelector('.gamepage__image').src = "/images/img_win.png";
+  } else if(incorrectGuesses === 1) {
+    document.querySelector('.gamepage__image').src = "/images/img2_head.png";
+  } else if(incorrectGuesses === 2) {
+    document.querySelector('.gamepage__image').src = "/images/img3_head_body.png";
+  } else if(incorrectGuesses === 3) {
+    document.querySelector('.gamepage__image').src = "/images/img4_one_arm.png";
+  } else if(incorrectGuesses === 4) {
+    document.querySelector('.gamepage__image').src = "/images/img5_two_arms.png";
+  } else if(incorrectGuesses === 5) {
+    document.querySelector('.gamepage__image').src = "/images/img6_one_leg.png";
+  } else if(incorrectGuesses === 6) {
+    document.querySelector('.gamepage__image').src = "/images/img7_lose.png"; 
+    alert("You've Lost!!");
+    displayedAnswerState.innerHTML = word;
+  } 
 }
   
 
